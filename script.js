f2270561a61a6015c42a5a4e4e1907461ef46dbc
Gchin99 <gchin99@gmail.com>
2022-07-24 22:50:19 -0700

    solved array challenges

--- script.js
+++ script.js
@@ -34,3 +34,55 @@
 //
 // var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
 // console.log(result); // we expect back [6, 8, 10, 9]
+
+// 3. Better than average: Given an array of numbers return a count of how many of the numbers are larger than the average.
+// function betterThanAverage(arr) {
+//     var count = 0
+//     const sum = 0;
+    
+//     const sumWithInitial = arr.reduce(
+//         (previousValue, currentValue) => previousValue + currentValue,
+//         sum
+//     );
+//     console.log(sumWithInitial);
+//     var avg = sumWithInitial/arr.length;
+//     console.log(avg);
+
+//     var count = 0
+//     for (var i=0; i<arr.length; i++){
+//         if (arr[i]>avg){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+// console.log(result); // we expect back 4
+
+
+// // 4. Array Reverse: Write a function that will reverse the values an array and return them.
+// function reverse(arr) {
+//     return arr.reverse();
+// }
+
+// var result = reverse(["a", "b", "c", "d", "e"]);
+// console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+
+// 5.Fibonacci Array: Fibonacci numbers have been studied for years and appear often in nature. 
+// Write a function that will return an array of Fibonacci numbers up to a given length n. 
+// Fibonacci numbers are calculated by adding the last two values in the sequence together. 
+// So if the 4th value is 2 and the 5th value is 3 then the next value in the sequence is 5.
+function fibonacciArray(n) {
+
+    var fibArr = [0, 1];  // the [0, 1] are the starting values of the array to calculate the rest from
+    var data = [];
+
+for(var i = 2; i < n; i++) {
+    fibArr[i] = fibArr[i - 1] + fibArr[i - 2]; 
+    data.push(fibArr[i]);
+}
+    return fibArr;
+}
+
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
